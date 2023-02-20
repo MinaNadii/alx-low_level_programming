04ba3198f9380ffba8a8e88bce69278dc4a10699
MinaNadii <mina.nady@me.com>
2023-02-20 10:40:33 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: print all letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	for (c = "a"; c <= "z" ; c++
+		putchar(c)
+		putchar("\n");
+		
+	return (0);
+}
