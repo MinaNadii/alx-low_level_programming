7bb4d1290e370d7911009ec2cc48b5422fbcc25d
MinaNadii <mina.nady@me.com>
2023-02-20 07:24:29 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-	fput(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	printf(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 		return (1);
 }
