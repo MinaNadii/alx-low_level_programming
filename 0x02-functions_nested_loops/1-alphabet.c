a5ef5387a671630961daf171aa667c3200fe2835
MinaNadii <mina.nady@me.com>
2023-02-22 16:47:32 +0200

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Description - alphabet a-z
+ * _putchar('\n')
+*/
+
+void print_alphabet(void);
+{
+int c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+_putchar('\n');
+}
