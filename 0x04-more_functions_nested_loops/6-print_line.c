d8fdf8f7c977e72c91173487e8a9144ee7d3f83c
MinaNadii <mina.nady@me.com>
2023-02-24 12:33:11 -0800

    2&3&4&5&6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,7 +12,7 @@
 
 void print_line(int n)
 {
-	int num_lines;
+	int num;
 
 	if (n <= 0)
 	{
@@ -20,7 +20,7 @@ void print_line(int n)
 	}
 	else
 	{
-		for (num_lines = 1; num_lines <= n; num_lines++)
+		for (num = 1; num_lines <= n; ++num)
 		{
 			_putchar('_');
 		}
