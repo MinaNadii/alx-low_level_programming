97de30aec5ce58916e38a8d0cf475fd10b29b2af
MinaNadii <mina.nady@me.com>
2023-02-28 14:37:25 +0200

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * void _puts -  prints a string to stdout
+ * _puts - prints a string to stdout
  * @str: pointer to the string to print
-*/
-
+ */
 void _puts(char *str)
 {
 	int i;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
